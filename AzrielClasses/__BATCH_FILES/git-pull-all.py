fb447dfe95c0e3ed2610a1bc4499d425f38a2aab
Azriel Berger <azrielbe@savion.huji.ac.il>
2025-09-29 14:22:25 +0300

    push new branch when pull-all

--- AzrielClasses/__BATCH_FILES/git-pull-all.py
+++ AzrielClasses/__BATCH_FILES/git-pull-all.py
@@ -2,10 +2,13 @@ import git # requires "GitPython"
 import os
 
 
-def run_os_command(cmd):
+def run_os_command(cmd, abort_for_any_error=False):
     print(f'\033[93m{cmd}\033[0m')
     exit_code = os.system(cmd)
     print()
+    if abort_for_any_error and exit_code != 0:
+        print(f"Error #{exit_code} has been occured!")
+        sys.exit(exit_code)
     return exit_code
 
 
@@ -17,7 +20,11 @@ except Exception as e:
     
 os.chdir(repo.git.rev_parse("--show-toplevel"))
 print()
-run_os_command("git pull")
+if run_os_command("git pull") != 0:
+    res = input("Do you want to push your branch? [Y]|[n] ").lower()
+    print()
+    if res != 'n':
+        run_os_command(f"git push --set-upstream origin {repo.active_branch.name}")
 for branch in repo.branches:
     if branch != repo.active_branch:
         exit_code = run_os_command(f"git fetch origin {branch.name}:{branch.name}") #we use the command line interface for printing the information
