94a6c4404af61054c1e7b32fda08a166f793fba0
Azriel Berger <azrielbe@savion.huji.ac.il>
2025-11-03 17:09:00 +0200

    improve printing in git-pull-all

--- AzrielClasses/__BATCH_FILES/git-pull-all.py
+++ AzrielClasses/__BATCH_FILES/git-pull-all.py
@@ -12,30 +12,36 @@ def run_os_command(cmd, abort_for_any_error=False):
     return exit_code
 
 
-try:
-    repo = git.repo.Repo('.', search_parent_directories=True)
-except Exception as e:
-    print(type(e),e)
-    repo = git.repo.Repo(fr"{os.path.dirname(__file__)}\..\..\.git")
-    
-os.chdir(repo.git.rev_parse("--show-toplevel"))
-print()
-if run_os_command("git pull") != 0:
-    res = input("Do you want to push your branch? [Y]|[n] ").lower()
+def main():
+    if os.name == 'nt':
+        os.system('') # thanks to ChatGPT that has given me this tip for using the coloring in `run_os_command`
     print()
-    if res != 'n':
-        run_os_command(f"git push --set-upstream origin {repo.active_branch.name}")
-for branch in repo.branches:
-    if branch != repo.active_branch:
-        exit_code = run_os_command(f"git fetch origin {branch.name}:{branch.name}") #we use the command line interface for printing the information
-        if exit_code != 0:
-            res = input("Do you want to delete this local branch? [y]|[N] ").lower()
-            print()
-            if res == 'y':
-                run_os_command("git branch -D " + branch.name)            
-run_os_command("git status")
-if 'use "git push"' in repo.git.status():
-    res = input("Do you want to push your changes? [Y]|[n] ").lower()
-    print()
-    if res != 'n':
-        run_os_command("git push")
+    try:
+        repo = git.repo.Repo('.', search_parent_directories=True)
+    except Exception as e:
+        print(type(e),e)
+        repo = git.repo.Repo(fr"{os.path.dirname(__file__)}\..\..\.git")
+        
+    os.chdir(repo.git.rev_parse("--show-toplevel"))
+    if run_os_command("git pull") != 0:
+        res = input("Do you want to push your branch? [Y]|[n] ").lower()
+        print()
+        if res != 'n':
+            run_os_command(f"git push --set-upstream origin {repo.active_branch.name}")
+    for branch in repo.branches:
+        if branch != repo.active_branch:
+            exit_code = run_os_command(f"git fetch origin {branch.name}:{branch.name}") #we use the command line interface for printing the information
+            if exit_code != 0:
+                res = input("Do you want to delete this local branch? [y]|[N] ").lower()
+                print()
+                if res == 'y':
+                    run_os_command("git branch -D " + branch.name)            
+    run_os_command("git status")
+    if 'use "git push"' in repo.git.status():
+        res = input("Do you want to push your changes? [Y]|[n] ").lower()
+        print()
+        if res != 'n':
+            run_os_command("git push")
+
+if __name__ == "__main__":
+    main()
